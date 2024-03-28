d9ba04d1c53bc2c3971d25f252f4f512ce9852f6
GUEMHAE <111509499+GUEMHAE@users.noreply.github.com>
2024-03-28 11:47:10 +0900

    GameUI배치

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // 싱글톤을 할당할 전역 변수
+    public bool isGameOver = false;
+    public Text scoreText;
+    public GameObject gameoverUI;
+
+
+    private int score = 0;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (isGameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    public void AddScore(int newScore)
+    {
+        if (isGameOver)
+        {
+            score += newScore;
+        }
+    }
+
+    public void OnPlayerDead()
+    {
+        isGameOver = true;
+        gameoverUI.SetActive(true);
+    }
+}
