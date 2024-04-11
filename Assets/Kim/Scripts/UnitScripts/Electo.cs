bf62b46603e2e9e8d35f7b159c3eb60aa3fc5754
dhdlqns12 <dhdlqns12@gmail.com>
2024-04-11 12:10:14 +0900

    모든 유닛 프리팹
    모든 유닛 프리팹

---
+++ Assets/Kim/Scripts/UnitScripts/Electo.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System;
+
+public class Electo : MonoBehaviour,IUnit
+{
+    public UnitInfo unitInfo; //UnitInfo스크립터블 오브젝트를 받아오기 위한 변수
+
+    public GameObject enemy; //가장 가까운 적 유닛을 담는 오브젝트
+    public float shortDis; //가장 가까운 적과의 거리를 저장하는 변수
+    public string tagName="Enemy"; //적의 태그 이름 초기화
+    public GameObject attackTarget;
+    public GameObject dummy; //멀리 떨어뜨린 더미 오브젝트 
+
+    public string unitName; //유닛 이름
+    public float attackSpeed; //공격 속도
+    public float attackRange; //공격 범위
+    public float ad; //유닛 ad
+    public float ap;  //유닛 ap
+    public float cost; //유닛 구매 가격
+    public float sellCost; //유닛 판매 가격
+    public GameObject attackProjectile; //유닛 공격 프로젝타일
+    public Transform attackSpawn; //유닛 공격 시작 위치
+
+    public string unitNameP
+    {
+        get => unitName;
+        set=>unitName = value;
+    }
+    public float attackSpeedP
+    {
+        get => attackSpeed;
+        set => attackSpeed = value;
+    }    
+    public float  attackRangeP
+    {
+        get => attackRange;
+        set => attackRange = value;
+    }
+    public float adP
+    {
+        get => ad;
+        set => ad= value;
+    }
+    public float apP
+    {
+        get => ap;
+        set => ap=value;
+    }
+    public float costP
+    {
+        get => cost;
+        set => cost = value;
+    }
+    public float sellCostP
+    {
+        get => sellCost;
+        set => sellCost = value;
+    }
+    public GameObject attackProjectileP
+    {
+        get => attackProjectile;
+        set => attackProjectile=value;
+    }
+    //Interface로 선언한 변수들을 정의하는 부분
+
+    void SpawnProjectile()
+    {
+        GameObject clone = Instantiate(attackProjectile, attackSpawn.position, Quaternion.identity); //프로젝타일은 attackSpawn위치에 생성
+        clone.GetComponent<AttackProjectile>().Targeting(enemy.transform);//가장 가까운 적을 타게팅함
+    }
+
+    async UniTask AttackToTarget()
+    {
+        while (true)
+        {
+            if (EnemySpawnManager.instance.EnemyPool.childCount == 0)
+            {
+                enemy = dummy;
+            }
+
+            if (enemy==null) //적이 null이면
+            {
+                await UniTask.WaitUntil(() => enemy != null);
+            }
+            if (Round.instance.isRound ==true && shortDis <= attackRange) //최소거리가 공격사거리보다 작거나 같다면
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(1 / attackSpeed)); //공격속도에 맞춰
+                Debug.Log("공격생성직전");
+                if (EnemySpawnManager.instance.EnemyPool.childCount == 0)
+                {
+                    enemy = dummy;//2라운드부터 enemy가 missing으로 변해 공격을 안하는 걸 막기 위한 코드
+                }
+                if (enemy != dummy)//더미가 아닐 경우만 공격
+                {
+                    SpawnProjectile(); //프로젝타일 생성
+                }
+            }
+            await UniTask.WaitUntil(() => enemy != null); //적이 null이 아닐때까지 다시 대기
+        }
+    }
+
+    void CheckEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(tagName);
+
+        shortDis = float.MaxValue;
+        foreach(GameObject enemyObject in enemies)
+        {
+            float distance = Vector3.Distance(gameObject.transform.position, enemyObject.transform.position);
+            if(distance<shortDis)
+            {
+                enemy = enemyObject;
+                shortDis = distance;
+            }
+        }
+    }
+
+    void Start()
+    {
+        AttackToTarget();
+    }
+
+    private void OnEnable()
+    {
+        unitNameP = unitInfo.UnitName;
+        attackSpeedP = unitInfo.AttackSpeed;
+        attackRangeP = unitInfo.AttackRange;
+        adP = unitInfo.Ad;
+        apP = unitInfo.Ap;
+        costP = unitInfo.Cost;
+        sellCostP = unitInfo.SellCost;
+        attackProjectileP = unitInfo.attackProjectile;
+    }
+
+    private void Update()
+    {
+        CheckEnemies();
+    }
+}
