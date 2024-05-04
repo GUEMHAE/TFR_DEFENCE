ddbaa556b2bfeb6430ec3051a4cd29e2ad1c84d5
ino <111509499+GUEMHAE@users.noreply.github.com>
2024-05-05 05:57:43 +0900

    유닛제한배치

--- Assets/Park/Scripts/UnitLimitManager.cs
+++ Assets/Park/Scripts/UnitLimitManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class UnitLimitManager : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> allUnits;
+    List<GameObject> allUnits; //유닛 리스트
     [SerializeField]
-    List<GameObject> allGrid;
+    List<GameObject> allGrid; //그리드 리스트
 
-    public static UnitLimitManager instance; //싱글톤 패턴
+    [SerializeField]
+    int unitCount; //그리드 위에 있는 유닛의 수 세는 변수
+
+    UnitLimitManager instance;
 
     public void GetOnlyGridUnits()
     {
@@ -21,23 +24,28 @@ public class UnitLimitManager : MonoBehaviour
             {
                 if (child != Grid.transform)
                 {
-                    allUnits.Add(child.gameObject);
+                    allUnits.Add(child.gameObject); //그리드 상의 자식 오브젝트를 받아서 allUnit(유닛 리스트)에 추가
                 }
             }
         }
     }
-    
-
 
     private void Awake()
     {
-        if (instance == null)
+        if(instance==null)
         {
             instance = this;
         }
-        if (instance != null)
+        else
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        GetOnlyGridUnits();
+        unitCount = allUnits.Count;
+        Debug.Log(unitCount);
     }
 }
