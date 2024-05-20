976958a2b57b95803a827972a231372c0725376b
dlcofls <cciicciiccii@naver.com>
2024-05-20 09:42:00 +0900

    esc ui

--- Assets/Lee/Scripts/ESCUI.cs
+++ Assets/Lee/Scripts/ESCUI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ESCUI : MonoBehaviour
 {
+    public GameObject escImage; // esc 이미지 표시할 게임 오브젝트
+    public bool isimage = false; // esc 이미지의 여부 저장
 
     public GameObject Shop;
     public bool isTab = false;
@@ -11,12 +13,30 @@ public class ESCUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        escImage.SetActive(false); // 이미지 비활성화
         Shop.SetActive(false); // 이미지 비활성화
     }
 
     // Update is called once per frame
     void Update()
     {
+                // ESC 키를 누르면 이미지를 활성화
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isimage = !escImage.activeSelf; // 활성화 상태 반전
+
+            if (isimage)
+            {
+                escImage.SetActive(true); // 활성화
+                Time.timeScale = 0;
+            }
+            else
+            {
+                escImage.SetActive(false); //비활성화
+                Time.timeScale = 1;
+            }
+        }
+
         // Tab 키를 누르면 이미지를 활성화
         if (Input.GetKeyDown(KeyCode.Tab))
         {
@@ -29,7 +49,11 @@ public class ESCUI : MonoBehaviour
             else
                 Shop.SetActive(false); //비활성화
         }
-
-
     }
+    public void OnButtonClick()
+    {
+        escImage.SetActive(false); //비활성화
+        Time.timeScale = 1;
+    }
+
 }
